a43a666aa2bc5e5a8bdb779c1466da00d3c7bcb5
Your Name <you@example.com>
2019-01-19 10:58:19 +0000

    11547

---
+++ 11547.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Suppress some compilation warning messages (only for VC++ users)
+#define _CRT_SECURE_NO_DEPRECATE
+//Shortcuts for "common" data types in contests
+typedef long long ll; //comments that are mixed in with code
+typedef pair<int, int> ii; //are aligned to the right like this
+typedef vector<ii> vii;
+typedef vector<int> vi;
+#define INF 1000000000 //1 billion, safer than 2B for Floyd Warshall's
+
+// Common memset settings
+//memset(memo, -1,  sizeof memo); //initialize DP memoization table with -1
+//memset(arr, 0, sizeof arr);     // to clear array of integers
+
+//several shorcuts that are frequently used:
+// ans = a ? b : c;
+// ans +=val
+// index = (index+1) %n;
+// index = (index + n - 1) % n;
+// int ans = (int) ((double)d + 0.5); //for rounding to nearest integer
+// ans = min(ans, new_computation);  //min/max shortcut // alternative form but not used in this book: ans<?= new_computation
+
+int main()
+{
+   int t;
+   scanf("%d", &t);
+   while(t--)
+   {
+	int n;
+	scanf("%d", &n);
+	n *= 567;
+	n /=9;
+	n +=7492;
+	n *=235;
+	n /= 47;
+	n -= 498;
+	printf("%d\n", abs((n/10)%10));
+   }
+   return 0;
+}
