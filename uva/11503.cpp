b7f0d0a61a9eacbf5cfb7c43e2ba3afd91f635a8
Your Name <you@example.com>
2019-07-01 15:49:52 -0500

    ..

--- uva/11503.cpp
+++ uva/11503.cpp
@@ -2,39 +2,63 @@
 using namespace std;
 
 map<string, string> parent;
-map<string, int> rank;
+map<string, int> Rank;
+map<string, int> size;
 
 string find(string node)
 {
 	if( parent[node] == node) return node;
 	else
-	   return find(parent[node]);
+	   return (parent[node] =find(parent[node]));
 }
-void unionset(string A, string B)
+int unionSet(string A, string B)
 {
 	string rootA = find(A);
 	string rootB = find(B);
-        if( rank[rootA] < rank[rootB] )
+        if( rootA == rootB ) return size[rootA];
+        if( Rank[rootA] < Rank[rootB] )
 	{
-            
+	   parent[rootA] = rootB; 
+	   size[rootB] += size[rootA];
+	    return size[rootB];
+	  
 	}
 	else
 	{
+	   parent[rootB] = rootA; 
+	   if( Rank[rootA] == Rank[rootB] ) Rank[rootA]++;
+	   size[rootA] += size[rootB];
+	   return size[rootA];
 	}
 }
 
 int main()
 {
- int n F;
+ int n, F;
  char nameA[21], nameB[21];
  scanf("%d\n", &n);
  for(int i = 0; i < n; i++)
     {
 	    scanf("%d\n", &F);
+	    parent.clear();
+	    Rank.clear();
+
 	    for(int j = 0; j < F; j++)
             {
 	       scanf("%s %s\n", nameA, nameB);
-	       unionSet(nameA, nameB);
+		if(!parent.count(nameA))
+		{
+		 parent[nameA]=nameA;
+		 Rank[nameA]=0;
+		 size[nameA]=1;
+		}
+		if(!parent.count(nameB))
+		{
+		 parent[nameB]=nameB;
+		 Rank[nameB]=0;
+		 size[nameB]=1;
+		}
+	        cout << unionSet(nameA, nameB) << endl;
 	    }
 	    
     }
