3373b065607e65f54c6dba811edd24c87f8d3b84
Joel <joel.chacon@cimat.mx>
2020-02-10 16:08:13 -0600

    ..

--- uva/481.cpp
+++ uva/481.cpp
@@ -1,52 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int prev[100000]={-1};
-int dp[100000]={0};
+int dp[100000];
 int data[100000];
 
-int DC(int L, int R, int value)
+int i = 0, size_dp = 1;
+int DC(int L, int R, int value) //like segment tree fashion..
 {
-  if( L == R) return L;
+  if( L > R)return 0;
+  if( L == R)
+  {
+   if( dp[L] > value)
+     dp[L] = value;
+   else if( dp[L] < value)
+     dp[size_dp++] = value;
+   return L;
+  }
   int m = (L+R)/2;
-  if(dp[m] < value) return DC(L, m);
-  else if(dp[m]> R) return DC(m+1, R);
+  if(dp[m] < value) return DC(m+1, R, value);
+  else if(dp[m] > value) return DC(L, m-1, value);
+  else return m;
 }
 int main()
 {
- int i = 1, size_dp = 1;
- dp[0] = -10000;
- prev[0] = -1;
+ dp[0]=10000000; 
  while(scanf("%d", &data[i]) != EOF)
  {
-    //looking for the index of LIS
-    int idx = DC(0, i-1, data[i]);
-    if( data[i] < dp[idx])
-    {
-	dp[idx] = data[i];
-        prev[idx] = dp[idx-1];
-    }
-    else
-    {
-	prev[size_dp] = dp[size_dp];
-	dp[size_dp] = data[i];
-	size_dp++;
-    }
-    i++;
+   DC(0, size_dp-1, data[i]);
+   i++;
  }
- 
- printf("%d\n-\n", size_dp);
- stack<int> st;
- st.push(data[maxidx]);
- int p = prev[maxidx];
- while( p != -1)
- {
-   st.push(data[p]);
-  p = prev[p];
- }
- while(!st.empty())
- {
-	printf("%d\n", st.top());
-	st.pop();
- }
- return 0;
+  printf("%d\n-\n", size_dp);
+  for(int i = 0; i < size_dp; i++) printf("%d\n", dp[i]);
+  return 0;
 }
