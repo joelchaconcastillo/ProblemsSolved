345ff20505c9bcbf23846960cf70d711ca2b64d6
jcc <joel.chacon@cimat.mx>
2020-02-10 12:22:49 -0600

    ..

--- uva/481.cpp
+++ uva/481.cpp
@@ -1,36 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+int prev[100000]={-1};
+int dp[100000]={0};
+int data[100000];
 
+int DC(int L, int R, int value)
+{
+  if( L == R) return L;
+  int m = (L+R)/2;
+  if(dp[m] < value) return DC(L, m);
+  else if(dp[m]> R) return DC(m+1, R);
+}
 int main()
 {
- int dp[100000]={0};
- int prev[100000]={-1};
- int data[100000];
- int i = 0, maxidx=0;
- dp[0] = 1;
- scanf("%d", &data[i++]);
+ int i = 1, size_dp = 1;
+ dp[0] = -10000;
+ prev[0] = -1;
  while(scanf("%d", &data[i]) != EOF)
  {
-    int maxdp = -1; 
-    for(int j = i-1; j >= 0; j--)
-	if( data[j] < data[i] && dp[j] > dp[maxdp]) 
-	       maxdp = j;
-    if( maxdp == -1)
+    //looking for the index of LIS
+    int idx = DC(0, i-1, data[i]);
+    if( data[i] < dp[idx])
     {
-	dp[i] = 1;
-	prev[i] = -1;
+	dp[idx] = data[i];
+        prev[idx] = dp[idx-1];
     }
     else
-    { 
-     dp[i] = dp[maxdp]+1;
-     prev[i] = maxdp;
+    {
+	prev[size_dp] = dp[size_dp];
+	dp[size_dp] = data[i];
+	size_dp++;
     }
-
-    if( dp[i] >= dp[maxidx]) 
-       maxidx = i;
     i++;
  }
- printf("%d\n-\n", dp[maxidx]);
+ 
+ printf("%d\n-\n", size_dp);
  stack<int> st;
  st.push(data[maxidx]);
  int p = prev[maxidx];
