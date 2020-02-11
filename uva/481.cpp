a852d747774d00c2505f7b09e2d6c83b817d1bf1
jcc <joel.chacon@cimat.mx>
2020-02-11 14:47:20 -0600

    ..

--- uva/481.cpp
+++ uva/481.cpp
@@ -1,34 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 int dp[100000];
+int preva[100000];
 int data[100000];
-
-int i = 0, size_dp = 1;
-int DC(int L, int R, int value) //like segment tree fashion..
+int i = 0, size_dp = 0;
+void DC(int L, int R, int value) //like segment tree fashion..
 {
-  if( L > R)return 0;
-  if( L == R)
+  if(L == R)
   {
-   if( dp[L] > value)
-     dp[L] = value;
-   else if( dp[L] < value)
-     dp[size_dp++] = value;
-   return L;
+   if( data[dp[L]] > value)
+    {
+     dp[L] = i;
+     preva[i] = dp[L-1];
+    }
+   else if( data[dp[L]] < value)
+    {
+     preva[i] = dp[size_dp-1];
+     dp[size_dp++] = i;
+    }
+   return;
   }
   int m = (L+R)/2;
-  if(dp[m] < value) return DC(m+1, R, value);
-  else if(dp[m] > value) return DC(L, m-1, value);
-  else return m;
+  if(data[dp[m]] < value) DC(m+1, R, value);
+  else if(data[dp[m]] > value) DC(L, m, value);
 }
 int main()
 {
- dp[0]=10000000; 
- while(scanf("%d", &data[i]) != EOF)
+ int value;
+ scanf("%d", &data[i++]);
+ dp[size_dp++] = 0;
+ preva[0]=-1;
+ while(scanf("%d", &value) != EOF)
  {
-   DC(0, size_dp-1, data[i]);
-   i++;
+   DC(0, size_dp-1, value);
+   data[i++] = value;
  }
   printf("%d\n-\n", size_dp);
-  for(int i = 0; i < size_dp; i++) printf("%d\n", dp[i]);
+ stack<int> st;
+ int prevv = dp[size_dp-1];
+// while(prevv != -1)
+ for(int k = 0; k < size_dp; k++)
+ {
+    st.push(data[prevv]);
+    prevv = preva[prevv];
+ }
+ while(!st.empty())
+{
+    printf("%d\n", st.top());
+    st.pop();
+}
   return 0;
 }
