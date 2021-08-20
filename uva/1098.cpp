bd8a007da3272272a563c9cf1af0c6d856745267
chaconcastillojoel <joel.chacon@cimat.mx>
2021-08-20 09:32:16 -0500

    ..

--- uva/1098.cpp
+++ uva/1098.cpp
@@ -2,13 +2,37 @@
 using namespace std;
 int rd[]={1, 0, -1, 0}, cd[]={0, -1, 0, 1}, ri[]={0,0,0,0}, ci[]={0,0,0,0}, ti[]={0,0,0,0};
 int n, m, cont;
-bool marked[8*8];
-int points[8*8];
-void bk(int r, int c, int d, int s)
+bool marked[8][8];
+int points[8][8];
+bool split(int x, int y){
+   static int v[9], i, j;
+   //left vertical line
+    v[0] = x-1 < 0 || y-1 < 0 || marked[x-1][y-1];
+    v[1] = x-1 < 0 || marked[x-1][y];
+    v[2] = x-1 < 0 || y+1 >= m || marked[x-1][y+1];
+    //botto horizontal mline
+    v[3] = y+1 >= m || marked[x][y+1];
+    //right vertical line
+    v[4] = x+1 >= n || y+1 >= m || marked[x+1][y+1];
+    v[5] = x+1 >= n || marked[x+1][y];
+    v[6] = x+1 >= n || y-1 < 0 || marked[x+1][y-1];
+    v[7] = y-1 < 0 || marked[x][y-1];
+    v[8] = v[0];
+    for(i = 0, j = 0; i < 8; i++)
+        if(v[i] != v[i+1])
+            j++;
+    return j >= 4;
+}
+void bk(int r, int c, int d)
 {
-   if(points[r*8+c]!=0 && points[r*8+c]!=d)return;
-   if(s<4 && (ti[s]-d) < (abs(ri[s]-r)+abs(ci[s]-c)) )return; 
-   if(points[r*8+c]!=0 && points[r*8+c]==d)s++;
+   if(points[r][c]!=0 && points[r][c]!=d)return;
+   for(int i = 0; i < 4; i++)
+   {
+      if(ti[i]==d && (r!=ri[i] || c!=ci[i]))return;
+      if(d < ti[i] && abs(r-ri[i])+abs(c-ci[i]) > ti[i]-d)return;
+   }
+   if(split(r, c))
+        return;
    if(d==m*n && r==0 && c==1){
      cont++; 
      return;
@@ -17,14 +41,17 @@ void bk(int r, int c, int d, int s)
    {
      int nr=r+rd[i], nc=c+cd[i]; 
      if(nr<0 || nc<0||nr>=n || nc>=m)continue;
-     if(marked[nr*m+nc])continue;
-     marked[nr*m+nc]=true;
-     bk(nr, nc, d+1, s);
-     marked[nr*m+nc]=false;
+     if(marked[nr][nc])continue;
+     marked[nr][nc]=true;
+     bk(nr, nc, d+1);
+     marked[nr][nc]=false;
    }
 }
 int main()
 {
+   ios_base::sync_with_stdio(false);
+   cin.tie(0);
+   cout.tie(0);
    int TC=1;
    while(cin>>n>>m)
    {
@@ -34,15 +61,15 @@ int main()
       for(int i = 0; i< 3; i++){
         cin>>ri[i]>>ci[i];
         ti[i] = floor(((i+1.0)/4.0)*(n*m));
-	points[ri[i]*8+ci[i]]=ti[i];
+	points[ri[i]][ci[i]]=ti[i];
       }
-      points[1]=n*m;
+      points[0][1]=n*m;
       ti[3] = n*m;
-      ri[3] = n-1;
-      ci[3] = m-1;
+      ri[3] = 0;
+      ci[3] = 1;
       cont = 0;
-      marked[0]=true;
-      bk(0,0,1,0);
+      marked[0][0]=true;
+      bk(0,0,1);
       cout <<"Case "<<TC++<<": "<<cont<<endl;
   }
    return 0;
